64411ccf8dee36a44f61096a1b3784f1565a3ef7
jeremie-codes <jrmmianda@gmail.com>
2026-06-27 14:06:54 +0100

    update

--- app/Http/Controllers/Api/ConversationController.php
+++ app/Http/Controllers/Api/ConversationController.php
@@ -91,8 +91,7 @@ class ConversationController extends Controller
                 $credit->available_messages <= 0
             ) {
                 return response()->json([
-                    'error' => 'no_available_messages',
-                    'message' => 'Vous n\'avez pas de forfait messages disponibles, veuillez acheter un forfait.',
+                    'message' => 'no_available_messages',
                 ],403);
             }
 
